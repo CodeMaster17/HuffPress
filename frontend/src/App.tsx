6c4cbbfc0775559ebefd2cc041381c2a48522cc8
Harshit Yadav <2105891@kiit.ac.in>
2024-09-20 22:25:49 +0530

    feat: basic setup for folder

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import Navbar from "./components/Navbar"
+import FileCompressorPage from "./pages/FileCompressorPage"
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <FileCompressorPage />
+    </>
+  )
+}
+
+export default App
---
+++ frontend/src/pages/FileCompressorPage.tsx
@@ -0,0 +1,13 @@
+
+const FileCompressorPage = () => {
+    return (
+        <>
+            <div>FileCompressorPage</div>
+            <h1 className="text-xl text-red-500 font-bold underline">
+                Hello world!
+            </h1>
+        </>
+    )
+}
+
+export default FileCompressorPage
